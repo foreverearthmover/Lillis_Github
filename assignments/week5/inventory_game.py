f9763fdb3476d54152a8dda1b480db6f48d6c2cf
foreverearthmover <lilliheidinger11@gmail.com>
2025-05-08 21:10:38 +0200

    finish game

--- assignments/week5/inventory_game.py
+++ assignments/week5/inventory_game.py
@@ -1,48 +1,58 @@
-#
+# This is a dungeon escape game.
+# You need to complete quests by picking up items to make it to the end.
 
 inventory = []
 MAX_INVENTORY_SIZE = 5
 items_in_room = []
 
 rooms = {
-    "room1": {
+    "first room": {
         "description": "A dimly lit room. You sense something hidden.",
         "items": [
             {"name": "Torch", "type": "tool", "description": "Lights up dark places."},
             {"name": "Apple", "type": "food", "description": "Restores a small amount of health."},
-            {"name": "Cat Treats", "type": "quest", "description": "Smells like tuna. A cat might like these."}
+            {"name": "Treats", "type": "quest", "description": "Smells like tuna. A cat might like these."}
         ],
-        "next": "room2"
+        "next": "second room"
     },
-    "room2": {
-        "description": "A room with ancient runes and glowing artifacts.",
+    "second room": {
+        "description": "It looks kinda magical in here... There's all kinds of embellished objects just laying"
+                       "\naround and even a strange and exotic-looking door. You can't see anything out of the window "
+                       "though.",
         "items": [
-            {"name": "Rune Stone", "type": "quest", "description": "Glows faintly."},
-            {"name": "Ancient Coin", "type": "quest", "description": "Stamped with a cat's face."},
-            {"name": "Crystal Shard", "type": "quest", "description": "Feels warm in your hand."}
+            {"name": "Rune Stone", "type": "quest", "description": "Glows faintly. It's cold and heavy."
+                                                                   "\nDon't drop it our it'll make a mess."},
+            {"name": "Ancient Coin", "type": "quest", "description": "You can make out a cat's face on it."
+                                                                     "\nThat's silly."},
+            {"name": "Crystal Shard", "type": "quest", "description": "Feels warm in your hand. It's even tingling."}
         ],
-        "next": "room3"
+        "next": "third room"
     },
-    "room3": {
-        "description": "A grand hall. A gigantic cat blocks your path.",
+    "third room": {
+        "description": "AHH! A gigantic cat! It's sitting right in the middle of grand hall, blocking your path. "
+                       "\nIt's eyes are locked on you... Are you it's prey...? "
+                       "\nDo something!",
         "items": [
-            {"name": "Mystic Dagger", "type": "weapon", "description": "Gleams with ancient energy. Could it defeat the cat?"}
+            {"name": "Mystic Dagger", "type": "weapon", "description": "Gleams with ancient energy..."
+                                                                       "Maybe this could defeat the cat?"}
         ],
         "next": None
     }
 }
 
 #tracks current room
-current_room = "room1"
+current_room = "first room"
 
 # --- Functions ---
 
 def prompt():
-    print("\tWelcome to my game!\n\n"
+    print("\tWelcome to my game!\n"
           "Explore the rooms to escape this dungeon.\n"
           "Type help for a list of commands.\n")
 
     input("Press Enter to continue...")
+    print("\n You're in a torch-lit room. There's just some ordinary stuff in front of you, "
+          "\nbut maybe you should take a closer look..\n")
 
 def show_inventory():
     # list all names of items in the inventory, consider the case when the list is empty
@@ -51,23 +61,24 @@ def show_inventory():
         for item in inventory:
             print(f"- {item['name']}")
     else:
-        print("You don't have any items in your inventory.")
+        print("There's nothing in your inventory.. You can pick up something from here.. or you don't...")
 
 def show_room_items():
     # list all items in current room
     room = rooms[current_room]
-    print(f"\nYou are in the {current_room} room. {room[description]}")
+    print(f"\nYou are in the {current_room}. {room["description"]}")
     if room["items"]:
         print("You see:")
         for item in room["items"]:
             print(f"- {item['name']}")
     else:
-        print("There are no items here.")
+        print("Nothing here.")
 
 def find_item(item_name, item_list):
     return next((item for item in item_list if item["name"].lower() == item_name.lower()), None)
 
 def pick_up(item_name):
+    # pick up an item from the room if inventory limit is not met yet, removed from room inv
     global inventory
     if len(inventory) >= MAX_INVENTORY_SIZE:
         print("Your inventory is full. You can't pick up any more items.")
@@ -76,10 +87,9 @@ def pick_up(item_name):
     if item:
         inventory.append(item)
         rooms[current_room]["items"].remove(item)
-        print(f"You picked up a {item['name']}.")
+        print(f"You picked up the {item['name']}.")
     else:
         print(f"There is no item called {item_name} here.")
-    # pick up an item from the room if inventory limit is not met yet, removed from room inv
 
 
 def drop(item_name):
@@ -89,30 +99,30 @@ def drop(item_name):
     if item:
         inventory.remove(item)
         rooms[current_room]["items"].append(item)
-        print(f"You dropped a {item['name']}.")
+        print(f"You dropped the {item['name']}.")
     else:
-        print(f"You don't have a {item_name} in your inventory.")
+        print(f"You don't have the {item_name} in your inventory.")
 
 def use(item_name):
     # Ex: use the item differently depends on the type
     global current_room
     item = find_item(item_name, inventory)
     if not item:
-        print(f"You don't have a {item_name} in your inventory.")
+        print(f"You don't have the {item_name} in your inventory.")
         return
 
     if item['type'] == "food":
         print(f"You eat the {item['name']}. It was tasty.")
         inventory.remove(item)
-    elif item['type'] == "weapon" and current_room == "room3":
+    elif item['type'] == "weapon" and current_room == "third room":
         print("You raise the Mystic Dagger to strike the cat...")
-        print("But before you do, the cat speaks!")
-        print("\n'Why did you try to attack me? I was sentient all along.'")
-        print("The cat vanishes. Your game ends in regret. You're locked out forever.\n")
+        print("But before you do.... the cat speaks!")
+        print("\n\"Why did you try to attack me??? I was just sitting here!! People have no manners these days.\"")
+        print("The cat vanishes. And with it... a door behind it does too.\n")
         exit()
-    elif item['type'] == "quest" and item['name'] == "Cat Treats" and current_room == "room3":
-        print("You offer the Cat Treats. The cat purrs and allows you to pass.")
-        print("You've completed the game peacefully. Well done!")
+    elif item['type'] == "quest" and item['name'] == "Treats" and current_room == "third room":
+        print("You offer the cat treats. The cat purrs and allows you to pass.")
+        print("That was so cute! Well done!")
         exit()
     else:
         print(f"You can't use the {item['name']} here.")
@@ -127,19 +137,20 @@ def examine(item_name):
         print(f"There is no {item_name} here.")
 
 def try_next_room():
+    # try to go to the next room
     global current_room
-    if current_room == "room1":
-        current_room = "room2"
+    if current_room == "first room":
+        current_room = "second room"
         print("You proceed to the next room.")
-    elif current_room == "room2":
+    elif current_room == "second room":
         required = {"Rune Stone", "Ancient Coin", "Crystal Shard"}
         if required.issubset(set(item["name"] for item in inventory)):
-            current_room = "room3"
-            print("The magical door opens as you gold the three items")
+            current_room = "third room"
+            print("The magical door opens as you hold the three special items")
         else:
             print("You can't go further. You need to find the required items.")
 
-    elif current_room == "room3":
+    elif current_room == "third room":
         print("The cat blocks your path. You can't go further if you don't use or offer something to interact.")
 
 # --- Game Loop ---
@@ -150,7 +161,8 @@ def game_loop():
         command = input("\n> ").strip().lower()
         if command == "help":
             # You can also rename the commands according to your own needs
-            print("Commands: inventory, look, pickup [item], drop [item], use [item], examine [item], quit")
+            print("Commands: inventory, look, pickup [item], drop [item], use [item], examine [item], go [to the "
+                  "next room], quit")
         elif command == "inventory":
             show_inventory()
         elif command == "look":
@@ -167,6 +179,8 @@ def game_loop():
         elif command.startswith("examine "):
             item_name = command[8:]
             examine(item_name)
+        elif command == "go":
+            try_next_room()
         elif command == "quit":
             print("Thanks for playing!")
             break
